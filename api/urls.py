2795c45de258ffccc1b9d94c973825261b89787e
illia.teodorov <illia.teodorov@globallogic.com>
2025-06-01 22:23:36 +0300

    Add caching

--- api/urls.py
+++ api/urls.py
@@ -2,10 +2,11 @@ from django.urls import include, path
 from rest_framework import routers
 
 from api import views
-from api.views import SearchAPIView
+from api.views import SearchAPIView, clear_search_endpoint_cache
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('search', SearchAPIView.as_view()),
+    path("clear-cache/", clear_search_endpoint_cache, name="clear-search-cache"),
 ]
--- config/urls.py
+++ config/urls.py
@@ -14,11 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from typing import Any
-
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import permissions
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
@@ -27,9 +24,6 @@ from drf_spectacular.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    # OpenAPI-схема в JSON/YAML
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
-    # Swagger-UI знизу бере URL='schema'
     path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    # наш Search ендпоїнт
 ]
